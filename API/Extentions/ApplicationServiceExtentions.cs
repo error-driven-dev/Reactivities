d4c92ae5dc01823f8728e302e65a4c8937312356
Beth Hart <errordrivendev@gmail.com>
2021-06-18 15:20:49 -0400

    refactor startup class serverices out into extention method

---
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,47 @@
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Persistance;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+
+namespace API.Extentions
+{
+    public static class ApplicationServiceExtentions
+    {
+ //static class do config has to be passed in from caller
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config){
+
+               
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opts => {
+                opts.AddPolicy("CorsDevPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyOrigin()
+                    .WithOrigins("http://localhost:3000");
+                });
+                
+            });
+            
+          services.AddMediatR(typeof(List.ActivityHandler).Assembly);
+          services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+          return services;
+            
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,14 +1,9 @@
+using API.Extentions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Persistance;
-using MediatR;
-using Application.Activities;
-using Application.Core;
 
 namespace API
 {
@@ -25,26 +20,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opts => {
-                opts.AddPolicy("CorsDevPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyOrigin()
-                    .WithOrigins("http://localhost:3000");
-                });
-                
-            });
-            
-          services.AddMediatR(typeof(List.ActivityHandler).Assembly);
-          services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+    services.AddControllers();
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
